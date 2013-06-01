0b0b8a4db24ea6e937b434af443298206e6d1372
EmilioTyl <emilio.tyl@gmail.com>
2013-05-31 23:22:54 -0300

    algunos comentarios

--- tilesFront.h
+++ tilesFront.h
@@ -31,27 +31,43 @@ enum estados {OFF=0,ON};
 typedef enum estados TipoEstado;
 typedef TipoEstado TipoFlag[6];
 
-void Menu (TipoDatos * dato,TipoFlag Flags,FILE **archivo_bitacora);
-void AccionesDeJuego(TipoDatos * dato,TipoFlag Flags,FILE * archivo_bitacora,TipoDatos * aux_dato);
+void Menu (TipoDatos * dato,TipoFlag Flags,FILE **archivo_bitacora);  // Pide informacion al usuario a traves de interfas grafica para setear los datos iniciales del juego
+
+
+void AccionesDeJuego(TipoDatos * dato,TipoFlag Flags,FILE * archivo_bitacora,TipoDatos * aux_dato);/* Esta funcion se encaraga de llamar a funciones del back en funcion
+                                                                                                      a la accion que ingresa el usuario */
+
 void resultadoFindelNivel(TipoDatos * dato,TipoFlag Flags);
 
-void PedidoDimenciones(TipoDatos * dato);
+void PedidoDimenciones(TipoDatos * dato); // Pide al usuario que ingrese las dimensiones de la matriz y setea las variables correspondientes
+
 void PedidoNivel(TipoDatos * dato);
-void PedirNombreValido(char * nombrefile);
+
+void PedirNombreValido(char * nombrefile); 
+
 int validFileName(char * nombrefile);
+
 void inputString(char * string);
-TipoEstado SaveBitacora(char * nombrefile,FILE * arch_origen);
-TipoEstado LoadBitacora(char * nombrefile,FILE ** archivo_bitacora);
+
+TipoEstado SaveBitacora(char * nombrefile,FILE * arch_origen); // Genera un archivo de texto con la copia del contenido arch_origen
+
+TipoEstado LoadBitacora(char * nombrefile,FILE ** archivo_bitacora);// Carga el archivo nombrefile con extension txt (si existe) modificando archivo_bitacora
 
 void imprimeTablero(TipoTablero * tablero);
+
 void imprimirEstado(TipoDatos * dato);
+
 void printerror(int ind);
+
 void instrucciones(void);
 
 int save(TipoDatos * dato, TipoEstado flagBitacora, char * nombre);
+
 int load(TipoDatos * dato, TipoEstado * flagBitacora, char * nombre);
-int GuardarAccionBitacora(FILE * archivo_bitacora,char * operacion, TipoEstado prox_nivel, int puntaje,int cant_azulejos);
-int GuardarMATBitacora(TipoTablero * tablero,FILE * archivo_bitacora);
+
+int GuardarAccionBitacora(FILE * archivo_bitacora,char * operacion, TipoEstado prox_nivel, int puntaje,int cant_azulejos); // Guarda en archivo_bitacora la accion ejecutada
+
+int GuardarMATBitacora(TipoTablero * tablero,FILE * archivo_bitacora); // Guarda en archivo_bitacora el tablero de juego
 
 void imprimirColor(char letra);
 
