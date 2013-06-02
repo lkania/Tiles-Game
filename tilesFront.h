0a3c3aad46356ecc8f7cf6f93bc768acc4165ac0
lkania <kania.lucas@gmail.com>
2013-06-02 19:15:11 -0300

    Agregada la documentacion

--- tilesFront.h
+++ tilesFront.h
@@ -24,58 +24,73 @@
 #define BITACORA			0 
 #define PROX_NIVEL			1
 #define FIN_JUEGO			2
-#define FIN_APLICACION		3
+#define FIN_APLICACION			3
 #define	UNDO				4
-#define LOAD                5
+#define LOAD				5
 
 typedef struct {
     FILE * archivo_bitacora;
     char nombre_bitacora[MAX_LONG_FILE+4];
 } TipoBitacora;
 
-
 enum estados {OFF=0,ON};
 typedef enum estados TipoEstado;
 typedef TipoEstado TipoFlag[6];
 
-void Menu (TipoDatos * dato,TipoFlag Flags,TipoBitacora * bitacora );  // Pide informacion al usuario a traves de interfas grafica para setear los datos iniciales del juego
+/* Pide informacion al usuario a traves de una interface grafica para setear los datos iniciales del juego */
+void Menu (TipoDatos * dato,TipoFlag Flags,TipoBitacora * bitacora );
 
-
-void AccionesDeJuego(TipoDatos * dato,TipoFlag Flags,TipoBitacora  bitacora,TipoDatos * aux_dato);/* Esta funcion se encaraga de llamar a funciones del back en funcion
-                                                                                                      a la accion que ingresa el usuario */
+/* Llama a funciones del BackEnd en funcion a la accion que ingresa el usuario */
+void AccionesDeJuego(TipoDatos * dato,TipoFlag Flags,TipoBitacora  bitacora,TipoDatos * aux_dato);
 
 void resultadoFindelNivel(TipoDatos * dato,TipoFlag Flags);
 
-void PedidoDimenciones(TipoDatos * dato); // Pide al usuario que ingrese las dimensiones de la matriz y setea las variables correspondientes
+/* Pide al usuario que ingrese las dimensiones de la matriz y setea las variables correspondientes */
+void PedidoDimenciones(TipoDatos * dato); 
 
+/* Pide al usuario que ingrese el nivel hasta el que desea jugar y setea las variables correspondientes */
 void PedidoNivel(TipoDatos * dato);
 
+/* Pide al usuario un nombre valido para un archivo y lo almacena en nombrefile */
 void PedirNombreValido(char * nombrefile); 
 
+/* Valida que nombrefile sea un nombre de archivo valido */
 int validFileName(char * nombrefile);
 
+/* Quita los '\n' finales de un string' */
 void inputString(char * string);
 
-TipoEstado SaveBitacora(char * nombrefile,FILE * arch_origen); // Genera un archivo de texto con la copia del contenido arch_origen
+/* Genera un archivo de texto con la copia del contenido arch_origen */ 
+TipoEstado SaveBitacora(char * nombrefile,FILE * arch_origen);
 
-TipoEstado LoadBitacora(char * nombrefile,FILE ** archivo_bitacora);// Carga el archivo nombrefile con extension txt (si existe) modificando archivo_bitacora
+/* Carga el archivo nombrefile con extension txt (si existe) modificando archivo_bitacora */
+TipoEstado LoadBitacora(char * nombrefile,FILE ** archivo_bitacora); 
 
+/* Imprime la matriz de juego */
 void imprimeTablero(TipoTablero * tablero);
 
+/* Imprime la cantidad actual de habilidades y el puntaje obtenido hasta el momento */
 void imprimirEstado(TipoDatos * dato);
 
+/* Imprime un mensaje correspondiente al error que se le pasa por parametro */
 void printerror(int ind);
 
+/* Imprime una pantalla explicando como se juega el juego y cuales son las dimensiones maximas que una matriz puede tener */
 void instrucciones(void);
 
+/* Guarda la partida que esta en juego */
 int save(TipoDatos * dato, TipoEstado flagBitacora, char * nombre);
 
+/* Carga un partida anteriormente salvada */
 int load(TipoDatos * dato, TipoEstado * flagBitacora, char * nombre);
 
-int GuardarAccionBitacora(FILE * archivo_bitacora,char * operacion, TipoEstado prox_nivel, int puntaje,int cant_azulejos); // Guarda en archivo_bitacora la accion ejecutada
+/* Guarda en archivo_bitacora la accion ejecutada */
+int GuardarAccionBitacora(FILE * archivo_bitacora,char * operacion, TipoEstado prox_nivel, int puntaje,int cant_azulejos);
 
-int GuardarMATBitacora(TipoTablero * tablero,FILE * archivo_bitacora); // Guarda en archivo_bitacora el tablero de juego
+/* Guarda en archivo_bitacora el tablero de juego */
+int GuardarMATBitacora(TipoTablero * tablero,FILE * archivo_bitacora);
 
+/* Imprime un char en color en la STDOUT*/
 void imprimirColor(char letra);
 
 #endif
