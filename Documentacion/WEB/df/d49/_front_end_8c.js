218f95b5655644b725cd911aae5ec941377a13ad
lkania <kania.lucas@gmail.com>
2013-06-07 01:11:10 -0300

    NEW DOCUMENTATION

---
+++ Documentacion/WEB/df/d49/_front_end_8c.js
@@ -0,0 +1,25 @@
+var _front_end_8c =
+[
+    [ "AccionesDeJuego", "df/d49/_front_end_8c.html#ab336c463d2464cd3ca4bb0f653e93efd", null ],
+    [ "cambiarColor", "df/d49/_front_end_8c.html#ac5da14380ebbd7eb58146932f909ec19", null ],
+    [ "compBit_File", "df/d49/_front_end_8c.html#ab10f46bf0cd9897c47a726d653728622", null ],
+    [ "GuardarAccionBitacora", "df/d49/_front_end_8c.html#ad75d779851077e6fb6b2fdace3b9f899", null ],
+    [ "GuardarMATBitacora", "df/d49/_front_end_8c.html#a330ec1027100b0b1f0829b22ee5a5415", null ],
+    [ "imprimeTablero", "df/d49/_front_end_8c.html#a9ef75758f3b47f3c2639f7bb586d0375", null ],
+    [ "imprimirColor", "df/d49/_front_end_8c.html#ad30237987566e160cc3530480d4514fc", null ],
+    [ "imprimirEstado", "df/d49/_front_end_8c.html#aa9153d7104d4019dd7aaf1e8a5718445", null ],
+    [ "inputString", "df/d49/_front_end_8c.html#a031f798e86eaa72898a0db976fc4566d", null ],
+    [ "instrucciones", "df/d49/_front_end_8c.html#a1122e9d0c8a9c1c2fd33acf80cb51e44", null ],
+    [ "load", "df/d49/_front_end_8c.html#a4e81936deabcba0a90039b3d8fd54a27", null ],
+    [ "main", "df/d49/_front_end_8c.html#a840291bc02cba5474a4cb46a9b9566fe", null ],
+    [ "Menu", "df/d49/_front_end_8c.html#aab334a05bc64bef008da24cca3fd83f2", null ],
+    [ "PedidoDimenciones", "df/d49/_front_end_8c.html#adf030e2911563c8b12da80699b53277d", null ],
+    [ "PedidoNivel", "df/d49/_front_end_8c.html#a767a1e350b410fc29bc0fc90012f1be9", null ],
+    [ "PedirNombreValido", "df/d49/_front_end_8c.html#a34256005c9f46eff46dddfaf74a73427", null ],
+    [ "Presentacion", "df/d49/_front_end_8c.html#ad02d883b4d6a33d8c18505cb21bc1c99", null ],
+    [ "printerror", "df/d49/_front_end_8c.html#ade56234c677e9f2fddee149cda63a52b", null ],
+    [ "resultadoFindelNivel", "df/d49/_front_end_8c.html#aa2c9f30c45a8afbc629eca6c5866cc00", null ],
+    [ "save", "df/d49/_front_end_8c.html#a6323e80bca186d3d9839b6a470500a98", null ],
+    [ "SaveBitacora", "df/d49/_front_end_8c.html#a75aefb8f0ed536d9adae0d1722d559e6", null ],
+    [ "validFileName", "df/d49/_front_end_8c.html#a639b04a4f1cdc236e6d0ffbd51db0c95", null ]
+];
---
+++ Documentacion/WEB/dir_937900b87e0c7a5fa01190c395fb83f7.js
@@ -0,0 +1,9 @@
+var dir_937900b87e0c7a5fa01190c395fb83f7 =
+[
+    [ "BackEnd.c", "d0/d58/_back_end_8c.html", "d0/d58/_back_end_8c" ],
+    [ "FrontEnd.c", "df/d49/_front_end_8c.html", "df/d49/_front_end_8c" ],
+    [ "getnum.c", "d4/d3f/getnum_8c.html", "d4/d3f/getnum_8c" ],
+    [ "getnum.h", "da/de1/getnum_8h.html", "da/de1/getnum_8h" ],
+    [ "tilesBack.h", "d5/dd5/tiles_back_8h.html", "d5/dd5/tiles_back_8h" ],
+    [ "tilesFront.h", "d5/dd8/tiles_front_8h.html", "d5/dd8/tiles_front_8h" ]
+];
